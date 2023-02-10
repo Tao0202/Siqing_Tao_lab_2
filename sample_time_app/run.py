3b44ef303fd94dae960076139cfbe6ca4b69ccad
tao0202 <tao@ktechboston.com>
2023-02-10 17:45:31 -0500

    add get time function to return current time

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+
+@app.route('/time')
+def get_time():
+    # ct stores current time
+    ct = datetime.now()
+    return ct
 
 
 app.run(host='0.0.0.0',
